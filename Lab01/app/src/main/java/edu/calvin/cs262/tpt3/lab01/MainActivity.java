22417aae0420caf278a815f0235f2b45cb03e927
Theron Tjapkes <theron@zmagnet.com>
2018-09-21 17:11:16 -0400

    Add Lab01

---
+++ Lab01/app/src/main/java/edu/calvin/cs262/tpt3/lab01/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.calvin.cs262.tpt3.lab01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
