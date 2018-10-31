0679f468d83dda4d06f754a06d88ad1981cb0770
Theron Tjapkes <theron@zmagnet.com>
2018-10-30 21:25:09 -0400

    Complete Lab08

--- Lab08/commands.sql
+++ Lab08/commands.sql
@@ -16,9 +16,24 @@ SELECT Player.ID from Player, PlayerGame
 SELECT * FROM Player
     WHERE emailAddress LIKE '%gmail.edu' */
 
--- Exercise 8.2
+
+/* -- Exercise 8.2
 
 -- Retrieve all “The King”’s game scores in decreasing order.
+SELECT PlayerGame.score FROM PlayerGame, Player
+    WHERE PlayerGame.playerID = Player.ID
+    AND Player.name = 'The King'
+    ORDER BY score DESC;
 -- Retrieve the name of the winner of the game played on 2006-06-28 13:20:00.
+SELECT Player.name FROM PlayerGame, Game, Player
+    WHERE PlayerGame.score = (
+        SELECT MAX(PlayerGame.score) FROM PlayerGame, Player, Game
+            WHERE Game.ID = PlayerGame.gameID
+            AND Player.ID = PlayerGame.playerID
+            AND Game.time = '2006-06-28 13:20:00'
+    )
 -- So what does that P1.ID < P2.ID clause do in the last example query?
+-- Since P1 and P2 are the same table there needs to be a check to make sure you don't match a value to itself, and not to match the same values twice
 -- The query that joined the Player table to itself seems rather contrived. Can you think of a realistic situation in which you’d want to join a table to itself?
+-- If you have a table of parts and there are 2 of the same part from different manufacturers, you might want to list all of the alternative parts from other manufacturers
+ */
